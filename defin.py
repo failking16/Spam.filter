3d058acacdf8e81aacc6cc4f8f8afa1796c70200
failking16 <66196978+failking16@users.noreply.github.com>
2023-03-17 14:52:07 +0600

    I have done some changes and I am taking break from this project to start new one and then I will continue again this project

--- defin.py
+++ defin.py
@@ -8,38 +8,62 @@ Created on Tue Feb 14 11:09:52 2023
 import numpy as np
 import pandas as pd
 import random
+import nltk
 import re
+from sklearn.feature_extraction.text import CountVectorizer
 
+
+def CountVector(array,size):
+    cv = CountVectorizer()
+    cv.fit_transform(array)
+    
 
 def erasing(name):
     f = open('Data/'+name,"w+")
     f.close()
 
 
+def remove_prepositions(text):
+    prepositions = ['at', 'by', 'for', 'from', 'in', 'of', 'on', 'to', 'with','the','a','are','is','am','but','and','or','out']
+    words = text.split()
+    filtered_words = [word for word in words if word.lower() not in prepositions]
+    return ' '.join(filtered_words)
+
+
 def randomiser(min,max):
-    i=0
-    index_list=np.array([])
-    
-    for i in range(10):
-        rand=random.randint(min,max)
-        index_list= np.append(index_list,rand)
-        
-    return index_list
+    arr = np.array([random.randint(min, max) for _ in range(10)])
+    return arr
 
 
 def cleaning_1():
     df = pd.read_csv('Data\emails.csv')
     df['spam'] = df['spam'].replace([1,''],0)
-    df = df.replace(['/','-','\+',':',';','!','  ','   ','the','The','and','or','not',],' ',regex=True)
     df['text'] = df['text'].str[8:]
     x = np.array(df['text'])
     number = r'[0-9]'
     length=0
     for length in range(len(df['text'])):
         x[length]=x[length].lower()
+        x[length]=remove_prepositions(x[length])
         x[length]=re.sub(number,'',x[length])
         length+=1
     df['text']=x
     df.to_csv('Data\change.csv')
+    
+    
+def cleaning_sample(array):
+    length=0
+    for length in range(len(array)):
+        array[length] = filter_english_words(array[length])
+        length+=1
+    return array
 
 
+def filter_english_words(text):
+    nltk.download('words')
+    words = text.split()
+    english_words = [word for word in words if word.lower() in nltk.corpus.words.words()]
+    return ' '.join(english_words)
+
+def countng(vocabulary):
+    
--- index searcher.py
+++ index searcher.py
@@ -9,15 +9,19 @@ import numpy as np
 import pandas as pd
 import defin as dfn
 import importlib
+from sklearn.feature_extraction.text import CountVectorizer
 
 importlib.reload(dfn)
-df = pd.read_csv('Data\emails.csv')
 
 dfn.erasing('index.csv')
 dfn.cleaning_1()
+cv = CountVectorizer()
+
+df = pd.read_csv('Data\emails.csv')
+cf = pd.read_csv('Data\change.csv')
 
 spam_indeces = np.array(df['spam'])
-spam_context = np.array(df['text'])
+spam_context = np.array(cf['text'])
 
 index = 0
 length = 0
@@ -30,6 +34,24 @@ for length in range(len(spam_indeces)):
 
 spam_example = dfn.randomiser(0,length)
 spam_set = dfn.randomiser(length+1,len(spam_indeces)-1)
-spam_tracker = np.array([])
+spam_example_text = spam_context[spam_example]
 
 print("\nYour spam indexes:\n",spam_example)
+print(spam_example_text[1])
+spam_contexts=dfn.cleaning_sample(spam_example_text)
+
+print(spam_example_text[1])
+
+for index in range(10):
+    spam_example_text[index]= spam_example_text[index]
+    index+=1
+
+Spam_list = list(spam_example_text)
+cv.fit_transform(Spam_list)
+
+filtered_vocab = {k:v for k,v in sorted(cv.vocabulary_.items(), key=lambda x:x[1]) if v >= 80}
+Z= np.array(filtered_vocab.keys())
+Y= np.array(filtered_vocab.values())
+
+
+
